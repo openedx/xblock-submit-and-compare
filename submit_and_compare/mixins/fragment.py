28751ed01c4d4ae5f10f9ddee710e76f8b8ae640
M. Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2020-06-26 10:29:41 +0500

    Run 2to3 (#6)

--- submit_and_compare/mixins/fragment.py
+++ submit_and_compare/mixins/fragment.py
@@ -4,8 +4,6 @@ Mixin fragment/html behavior into XBlocks
 Note: We should resume test coverage for all lines in this file once
 split into its own library.
 """
-from __future__ import absolute_import
-
 from django.template.context import Context
 from xblock.core import XBlock
 from web_fragments.fragment import Fragment
